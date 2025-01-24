9cdbfc6b9aecbaa445bc7d748a57e37e96884dcc
janker1747 <142620449+janker1747@users.noreply.github.com>
2025-01-24 21:26:34 +0300

    Add files via upload

--- Timer.cs
+++ Timer.cs
@@ -8,13 +8,14 @@ public class Timer : MonoBehaviour
 
     private float _count = 0f;
     private bool _isRunning;
+    private bool leftMouseButton = Input.GetMouseButtonDown(0);
     private Coroutine _coroutine;
 
     public event Action<int> CountChange;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (leftMouseButton)
         {
             HandleMouseButtonDown();
         }
